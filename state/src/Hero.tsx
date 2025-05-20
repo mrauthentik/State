d7aec699b150f95ffe32652be9113cb69178fbe4
mrauthentik <umokeuchenna2020@gmail.com>
2025-05-20 12:35:54 +0100

    Enhance styling and functionality in App.css and Hero.tsx; add Music component

--- state/src/Hero.tsx
+++ state/src/Hero.tsx
@@ -1,15 +1,38 @@
-import React from "react";
+import React, {useEffect, useRef} from "react";
+import Typed from "typed.js";
 import img from './assets/logo/uche.jpg'
+
+
 export const Hero:React.FC = ()=>{
+  const typedRef = useRef<HTMLSpanElement>(null)
+
+  useEffect(()=>{
+    const typed = new Typed(typedRef.current, {
+       strings: [
+        "A Front-end Developer",
+        "A Pixel Perfect Front-end Engineer",
+        "A Creative UI Developer",
+        "A Passionate React Developer"
+      ],
+      typeSpeed: 60,
+      backSpeed: 30,
+      backDelay: 1500,
+      loop: true,
+      showCursor: true
+    })
+
+    return () =>{
+      typed.destroy()
+    }
+  }, [])
   return(
     <div className="hero-section">
         <div className="text-container">
-            <p><span>Hello</span> my name is <b>Uche</b> </p>
-            <p>Am a <span>Front-end</span> Developer</p>
-             <p className="mini">A Pixel Perfect Front-end Engineer</p>
+            <p>Hi, I'm <b>Uche</b> </p>
+            <p ><span ref={typedRef}></span></p>
              <p className="description">
-                I'm creative designer based in Nigeria, 
-                and I'm very passionate and dedicated to my work.
+                I'm creative designer, 
+                i can bring your <span>Designs</span> to <span>Life</span> 
              </p>
 
                 <div className="sub">
